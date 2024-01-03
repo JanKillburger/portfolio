af13bc4b1ef053e61ba8b99e064c17e6b419d037
Jan Killburger <jkillburger@gmail.com>
2024-01-03 20:47:02 +0100

    Added burger menu animation

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,6 @@
-import { HtmlParser } from '@angular/compiler';
-import { Component, EventEmitter, Output } from '@angular/core';
-import { concat } from 'rxjs';
+
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-menu',
@@ -10,7 +10,16 @@ import { concat } from 'rxjs';
 export class MenuComponent {
   bodyElement = document.querySelector('body');
   menuOpen = false;
-
+  isShown = false;
+  imgPath = "assets/img/";
+  currentBurgerIcn = 'url("' + this.imgPath + 'burger-menu.png")';
+  burgerMenuIcons = [
+    "burger-menu.png",
+    "burger-menu1.png",
+    "burger-menu2.png",
+    "burger-menu3.png",
+    "burger-menu4.png"
+  ];
   menuItems = [
     {
       text: "About me",
@@ -27,6 +36,26 @@ export class MenuComponent {
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+    if (this.menuOpen) {
+      this.isShown = true;
+    }
     this.bodyElement?.classList.toggle('is-hidden');
+    this.animateIcon(this.menuOpen);
+    if (!this.menuOpen) setTimeout(() => {
+      this.isShown = false;
+    }, 500);
+  }
+
+  animateIcon(isOpening: boolean) {
+    let interval = 100;
+    let icons = this.burgerMenuIcons;
+    if (!isOpening) icons = icons.reverse();
+    for (let i = 1; i < icons.length; i++) {
+      const icn = 'url("' + this.imgPath + icons[i] +'")';
+      setTimeout(() => {
+        this.currentBurgerIcn = icn;
+      },(i + 1) * interval);
+    }
+    if (!isOpening) icons = icons.reverse();
   }
 }
