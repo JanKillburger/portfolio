f6da9f96eefa5a9acb75b10599c52ff0b5a6666a
Jan Killburger <jkillburger@gmail.com>
2024-08-29 19:05:52 +0200

    Add portfolio projects

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -18,10 +18,26 @@ myProjects = [
   {
     title: "Join",
     skills: ["JavaScript","HTML","CSS", "Git"],
-    desc: "Task manager inspired by the Kanban system. Create and organize tasks using drag and drop functions, assign users and categories",
+    desc: "Task manager inspired by the Kanban system. Create and organize tasks using drag and drop functions, assign users and categories.",
     imgPath: "assets/img/portfolio-join.png",
     appLink: "https://join.jan-killburger.de",
     repLink: "https://github.com/JanKillburger/join"
+  },
+  {
+    title: "DA Bubble",
+    skills: ["Angular","Firebase", "Git"],
+    desc: "Chat app inspired by Slack. Create teams to communicate or chat personally with collegues.",
+    imgPath: "assets/img/dabubble.png",
+    appLink: "https://dabubble.jan-killburger.de",
+    repLink: "https://github.com/JanKillburger/DaBubble"
+  },
+  {
+    title: "Videoflix",
+    skills: ["Python","Django","Angular"],
+    desc: "Video streaming platform with Django admin panel to upload new videos with background processing to multiple resolutions.",
+    imgPath: "assets/img/videoflix.png",
+    appLink: "https://jan-killburger.developerakademie.org/",
+    repLink: "https://github.com/JanKillburger/videoflix-backend"
   }
 ]
 }
